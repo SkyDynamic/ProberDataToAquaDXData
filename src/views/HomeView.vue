ac8ca9a0e68a9c60e96efd6e399b47a619332522
SkyDynamic <tcplxw@outlook.com>
2025-04-15 15:21:56 +0800

    ?

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,24 @@ import TopBar from "@/components/TopBar.vue";
 <template>
   <TopBar class="fixed w-full h-16 z-2 border-solid b-0 b-b-1 border-[var(--el-border-color)]" />
   <el-scrollbar
-      class="fixed z-1 bottom-0 right-0 w-full md:pl-48 FIX"
+      class="fixed z-1 bottom-0 right-0 w-full FIX"
   >
     <div class="h-16 block">&nbsp;</div>
     <router-view />
   </el-scrollbar>
+  <el-footer
+      class="fixed z-1 bottom-0 right-0 w-full FIX"
+  >
+    <el-row justify="center">
+      <el-col :span="24">
+        <el-divider>
+          <span class="text-xs">
+            Develop by <a href="https://github.com/SkyDynamic" target="_blank">SkyDynamic</a>
+          </span>
+        </el-divider>
+      </el-col>
+    </el-row>
+  </el-footer>
 </template>
 
 <style scoped>
